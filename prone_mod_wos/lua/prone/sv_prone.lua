1bc5eb63077b321a687bc434c0e3275dea511b82
George Petrou <gspetrou98@gmail.com>
2020-06-25 22:03:41 -0400

    Remove player from prone on spawn

--- prone_mod_wos/lua/prone/sv_prone.lua
+++ prone_mod_wos/lua/prone/sv_prone.lua
@@ -4,6 +4,12 @@ hook.Add("DoPlayerDeath", "prone.ExitOnDeath", function(ply)
 	end
 end)
 
+hook.Add("PlayerSpawn", "prone.ExitOnDeath", function(ply)
+	if ply:IsProne() then
+		prone.Exit(ply)
+	end
+end)
+
 if prone.Config.FallDamageMultiplier ~= 1 then
 	hook.Add("GetFallDamage", "prone.FallDamage", function(ply, speed)
 		if ply:IsProne() then
